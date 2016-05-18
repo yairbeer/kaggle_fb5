3b9d5c8ed9907c7ff279bb380e13cba28555e71e
yaia <yaia1309@gmail.com>
2016-05-18 11:31:17 +0300

    some exploration

--- dist and sd correlation.py
+++ dist and sd correlation.py
@@ -3,6 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 places = pd.DataFrame.from_csv('places_loc_sqr_weight_2016-05-17-17-36.csv')
+plt.hist(places['n_persons'], bins=100)
+plt.show()
+
 train = pd.DataFrame.from_csv('train.csv')
 
 train = pd.merge(left=train, right=places, how='left', left_on='place_id', right_on=places.index.values)
@@ -10,11 +13,7 @@ print(train)
 train['dist'] = np.sqrt((train['x_x'].values - train['x_y'].values) ** 2 +
                         (train['y_x'].values - train['y_y'].values) ** 2)
 
-plt.hist(np.log(1 + train['dist'].values / train['accuracy'].values), bins=50, range=(0, 1))
-plt.show()
-
-np.random.seed(100)
-choices = np.random.choice(np.arange(train.shape[0]), 1000, replace=False)
-
-plt.plot(train['accuracy'].values[choices], train['dist'].values[choices], 'ko')
+hist_data = np.clip(np.log(train['dist'].values / train['accuracy'].values), -1000, 1000)
+print(hist_data)
+plt.hist(np.log(hist_data), bins=100)
 plt.show()
